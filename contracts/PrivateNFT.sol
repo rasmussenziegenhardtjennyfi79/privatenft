a4f70afa3a97c6dccb6f50dcfaa04cb214195f00
rasmussenziegenhardtjennyfi79 <rasmussenziegenhardtjennyfi79@hotmail.com>
2024-09-14 11:23:52 +0000

    Initialised

--- contracts/PrivateNFT.sol
+++ contracts/PrivateNFT.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
     constructor(address initialOwner)
-        ERC721("zzzz","zzz")
+        ERC721("zzz","zzz")
         Ownable(initialOwner)
     {}
 
